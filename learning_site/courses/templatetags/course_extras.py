ad102525f232e810afdf98f9e4a670a89fa5358a
mike-del <delgado.portland@gmail.com>
2017-03-20 17:11:54 -0700

    Updated templates with simple tags and filters.

--- learning_site/courses/templatetags/course_extras.py
+++ learning_site/courses/templatetags/course_extras.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils.safestring import mark_safe
+
+import markdown2
 
 from courses.models import Course
 
@@ -24,7 +27,13 @@ def time_estimate(word_count):
 	'''Estimates the number of minutes it will take to complete a step
 	based on the passed-in wordcount
 	'''
-	minutes = round(woord_count/20)
+	minutes = round(word_count/20)
 	return minutes
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+	'''Converts markdown text to HTML'''
+	html_body = markdown2.markdown(markdown_text)
+	return mark_safe(html_body) 
 
 
